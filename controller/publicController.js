8a341fd2e7587e288e5d06ea60a4c824ffbc97ab
wilbertmdp <wilbertanderson_2327240015@mhs.mdp.ac.id>
2025-10-25 18:00:56 +0700

    pertemuan 25 oktober
    tambah detail profil user yang login

--- controller/publicController.js
+++ controller/publicController.js
@@ -15,9 +15,11 @@ export const nama = (req, res) => {
 
 export const privateProfile = async (req, res) => {
     try{
+        const user = req.user;
+
         res.status(200).json({
             message : "Private profile berhasil di akses",
-            data : null
+            data : user,
         })
     }catch (error){
         res.status(500).json({
